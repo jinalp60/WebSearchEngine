79d053a6bc72f45cb710dae273c77059cb59d4d1
jinalp60 <jinal_p60@yahoo.com>
2022-03-25 17:19:17 -0400

    Implemented web crawler and word searcher

---
+++ src/WebSearchEngine/SearchWord.java
@@ -0,0 +1,96 @@
+package WebSearchEngine;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+import textprocessing.TST;
+
+public class SearchWord {
+
+	private String outputPathForHTML = "src/HTMLwebpages/";
+	private String outputPathForText = "src/Textwebpages/";
+
+	public TST<Integer> getTSTOfWordOccurrences(String fileName) throws IOException {
+
+		FileReader file = new FileReader(this.outputPathForText + fileName);
+		BufferedReader br = new BufferedReader(file);
+
+		StringBuilder sb = new StringBuilder();
+		String line = null;
+		while ((line = br.readLine()) != null) {
+			sb.append(line);
+		}
+		br.close();
+
+		String inputText = sb.toString();
+
+		StringTokenizer stringTokenizer = new StringTokenizer(inputText, ".,-#|&(){}[]= \n\t");
+
+		TST<Integer> tst = new TST<Integer>();
+
+		while (stringTokenizer.hasMoreTokens()) {
+			String currentToken = stringTokenizer.nextToken();
+			int currentOccurrences;
+			if (tst.get(currentToken) == null) {
+				currentOccurrences = 0;
+			} else {
+				currentOccurrences = tst.get(currentToken);
+			}
+			tst.put(currentToken, currentOccurrences + 1);
+		}
+		return tst;
+
+	}
+
+	public void searchForWord(String wordToSearch, HashMap<String, String> webPagesNames) {
+		System.out.println("searching for " + wordToSearch);
+
+		HashMap<String, Integer> textFilesFound = new HashMap<>();
+
+		try {
+			File[] textFiles = new File(this.outputPathForText).listFiles();
+
+			for (File file : textFiles) {
+				if (file.isFile()) {
+					String fileName = file.getName();
+					TST<Integer> tst = this.getTSTOfWordOccurrences(fileName);
+					Integer wordOccurrences = tst.get(wordToSearch);
+					if (wordOccurrences != null) {
+						textFilesFound.put(fileName, wordOccurrences);
+					}
+				}
+			}
+
+			if (textFilesFound.size() > 0) {
+				for (Map.Entry textFileEntry : textFilesFound.entrySet()) {
+
+					String textFileName = textFileEntry.getKey().toString();
+					String textFileNameWithoutExt;
+					int pos = textFileName.lastIndexOf(".");
+					if (pos > 0 && pos < (textFileName.length() - 1)) {
+						textFileNameWithoutExt = textFileName.substring(0, pos);
+					} else {
+						textFileNameWithoutExt = textFileName;
+					}
+
+					for (Map.Entry webPageEntry : webPagesNames.entrySet()) {
+						if (webPageEntry.getKey().equals(textFileNameWithoutExt)) {
+							System.out.println(webPageEntry.getValue() + "->" + textFileEntry.getValue());
+							break;
+						}
+					}
+				}
+			} else {
+				System.out.println("'" + wordToSearch + "' word couldn't be found");
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ src/WebSearchEngine/WebSearchEngine.java
@@ -0,0 +1,51 @@
+package WebSearchEngine;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+import WebSearchEngine.WebCrawler;
+
+public class WebSearchEngine {
+
+	public static void main(String args[]) {
+
+		try {
+			Scanner scanner = new Scanner(System.in);
+
+			System.out.println("Welcome to Web Search Engine !!\n");
+
+			System.out.println("-------------------------------------------------------------------\n");
+
+			System.out.println("Please enter the URL which you want to crawl: \n");
+
+			String urlToCrawl = scanner.next();
+
+			System.out.println("Crawling has started ...\n");
+
+			WebCrawler wc = new WebCrawler();
+			wc.getPageLinks(urlToCrawl);
+			System.out.println("Crawling has finished ...\n");
+			HashMap<String, String> webPagesNames = wc.getHTMLWebPages();
+			wc.HTMLToTextconverter();
+
+			SearchWord searchWord = new SearchWord();
+			while(true) {
+				System.out.println("------------------------------------------------------------\n");
+				System.out.println("Please enter the word which you want to search or type quit : \n");
+				String wordToSearch = scanner.next();
+				if(wordToSearch.equals("quit")) {
+					break;
+				} else {
+					searchWord.searchForWord(wordToSearch, webPagesNames);
+				}
+			} 
+			
+			System.out.println("Program ended");
+
+		} catch (Exception e) {
+			// handle exception
+			System.out.println("Exception occured " + e);
+		}
+
+	}
+}
