e6cc23e2dd17d41f244f1285906b3acce5317713
Zeel Patel <zeel.c.patel@gmail.coml>
2022-04-02 12:58:48 -0400

    Revert "Url pre loading feature added"
    This reverts commit a64f3f49935b28c88c8bf35d9b0ed69dcd46933f.

--- src/WebSearchEngine/WebSearchEngine.java
+++ src/WebSearchEngine/WebSearchEngine.java
@@ -11,74 +11,36 @@ public class WebSearchEngine {
 
 		try {
 			Scanner scanner = new Scanner(System.in);
-			HashMap<String, String> webPagesNames = null;
-			SearchWord searchWord = new SearchWord();
+
 			System.out.println("Welcome to Web Search Engine !!\n");
-			int option = 0;
-			
-			while(option != 3) {
-				System.out.println("Please choose from following options: \n");
-				System.out.println(" 1. Search Word \n 2. Crawl URL \n 3. Quit \n");
 
-				
-				try {
-					option = scanner.nextInt();
-				} catch (Exception e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+			System.out.println("-------------------------------------------------------------------\n");
+
+			System.out.println("Please enter the URL which you want to crawl: \n");
+
+			String urlToCrawl = scanner.next();
+
+			System.out.println("Crawling has started ...\n");
+
+			WebCrawler wc = new WebCrawler();
+			wc.getPageLinks(urlToCrawl);
+			System.out.println("Crawling has finished ...\n");
+			HashMap<String, String> webPagesNames = wc.getHTMLWebPages();
+			wc.HTMLToTextconverter();
+
+			SearchWord searchWord = new SearchWord();
+			while(true) {
+				System.out.println("------------------------------------------------------------\n");
+				System.out.println("Please enter the word which you want to search or type quit : \n");
+				String wordToSearch = scanner.next();
+				if(wordToSearch.equals("quit")) {
+					break;
+				} else {
+					searchWord.searchForWord(wordToSearch, webPagesNames);
 				}
-					switch(option){
-						case 1:
-							webPagesNames = UrlPreLoader.GetData();
-							while(true) {
-								System.out.println("------------------------------------------------------------\n");
-								System.out.println("Please enter the word which you want to search or type quit : \n");
-								String wordToSearch = scanner.next();
-								if(wordToSearch.equals("quit")) {
-									break;
-								} 
-								else {
-									searchWord.searchForWord(wordToSearch, webPagesNames);
-								}
-							}
-							break;
-		
-						case 2:
-							System.out.println("-------------------------------------------------------------------\n");
-							System.out.println("Please enter the URL which you want to crawl: \n");
-							String urlToCrawl = scanner.next();
-
-							System.out.println("Crawling has started ...\n");
-
-							WebCrawler wc = new WebCrawler();
-							wc.getPageLinks(urlToCrawl);
-							System.out.println("Crawling has finished ...\n");
-							webPagesNames = wc.getHTMLWebPages();
-							wc.HTMLToTextconverter();
-
-							while(true) {
-								System.out.println("------------------------------------------------------------\n");
-								System.out.println("Please enter the word which you want to search or type quit : \n");
-								String wordToSearch = scanner.next();
-								if(wordToSearch.equals("quit")) {
-									break;
-								} 
-								else {
-									searchWord.searchForWord(wordToSearch, webPagesNames);
-								}
-							} 
-							break;
-						case 3:
-							System.out.println("Program ended");
-							break;
-					
-						default:
-							System.out.println("Enter Valid Input");
-
-					}
-				
-			}
-
+			} 
+			
+			System.out.println("Program ended");
 
 		} catch (Exception e) {
 			// handle exception
