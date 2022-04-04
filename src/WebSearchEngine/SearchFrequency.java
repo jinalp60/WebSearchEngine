d8ea1392429bb230392d69c092d318fbed82eb86
Zeel Patel <zeel.c.patel@gmail.com>
2022-04-04 13:27:36 -0400

    Search frequncy feature added

---
+++ src/WebSearchEngine/SearchFrequency.java
@@ -0,0 +1,50 @@
+package WebSearchEngine;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class SearchFrequency {
+
+	public static void FreqCount(String keyword) throws IOException {
+		keyword.toLowerCase();	
+		FileReader fr = new FileReader("src/Wordsmemory.txt");  //Creation of File Reader object
+	    FileWriter fw = new FileWriter("src/Wordsmemory.txt", true);
+	    BufferedReader br = new BufferedReader(fr); //Creation of BufferedReader object
+	    String s1;     
+	    String input= keyword;   // Input word to be searched
+	    int count=0;   //Intialize the word to zero
+	    while((s1=br.readLine())!=null)   //Reading Content from the file
+	    {
+	       String[] wordsplit = s1.split(" ");  //Split the word using space
+	        for (String word : wordsplit) 
+	        {
+	               if (word.equals(input))   //Search for the given word
+	               {
+	                 count++;  //If Present increase the count by one
+	               }
+	              
+	        }
+	    }
+	    if(count!=0)  //Check for count not equal to zero
+	    {
+	       System.out.println("This word has been searched for "+count+ " times.");
+	       System.out.println("-------------------------------------------------");
+	       fw.write(keyword+"\n");
+	       fw.close();
+	    }
+	    else
+	    {
+	       System.out.println("The given word was not searched before. adding the word to the library.");
+	       System.out.println("-------------------------------------------------");
+
+	       fw.write(keyword+"\n");
+	       fw.close();
+	    }
+	    
+	       fr.close();
+	    }
+	}
+
--- src/WebSearchEngine/UrlPreLoad.java
+++ src/WebSearchEngine/UrlPreLoad.java
@@ -11,20 +11,20 @@ public class UrlPreLoad {
 		HashMap<String, String> webPagesNames = null;
 		HashMap<String, String> allPagesNames = new HashMap<>();
 
-		Scanner s = new Scanner(new File("src/WebpreloadURL.txt"));
-		ArrayList<String> list = new ArrayList<String>();
-		while (s.hasNext()){
-			list.add(s.next());
+		Scanner s = new Scanner(new File("src/WebpreloadURL.txt")); //Scanning the URL file
+		ArrayList<String> list = new ArrayList<String>(); //Initialising the array to process
+		while (s.hasNext()){	//Check if the txt file has next line
+			list.add(s.next());	//Add the line to the list array
 		}
-		s.close();
+		s.close();	//Closing the files
 
-		for(int i=0;i<list.size();i++)
+		for(int i=0;i<list.size();i++)  //loop for the size of the list
 		{
-			WebCrawler wc = new WebCrawler();
+			WebCrawler wc = new WebCrawler();  //crawl each page mentioned in the URL file
 			wc.getPageLinks(list.get(i));
-			webPagesNames = wc.getHTMLWebPages();
+			webPagesNames = wc.getHTMLWebPages();  //Store name in hashmap
 			wc.HTMLToTextconverter();
-			allPagesNames.putAll(webPagesNames);
+			allPagesNames.putAll(webPagesNames); //merge hashmap in one place
 //			for (Map.Entry<String,String> pair : webPagesNames.entrySet()) {
 //				allPagesNames.put(pair.getKey(), pair.getValue());
 ////				System.out.println(allPagesNames);
@@ -32,7 +32,7 @@ public class UrlPreLoad {
 //			}
 			
 		} 
-		return allPagesNames;
+		return allPagesNames; //Returning the hashmap with all the pages which are crawled
 
 		}
 }
